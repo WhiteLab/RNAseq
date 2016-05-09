a1add1bebdd7e29256d34f33a23044d1f3f66b1f
Ubuntu <ubuntu@mb-vip-rnaseq.novalocal>
2016-05-09 19:49:56 +0000

    bug fixes

--- utility/karyosort_genome.py
+++ utility/karyosort_genome.py
@@ -15,11 +15,13 @@ gdict = {}
 cur = ''
 for line in genome:
     if line[0] == ">":
-        cur = line[1:]
+        line = line.rstrip('\n')
+        # split on space banking on first entry being the desired
+        cur = line[1:].split()[0]
         gdict[cur] = []
     else:
         gdict[cur].append(line)
 genome.close()
 
 for chrom in clist:
-    sys.stdout.wite('>' + chrom + '\n' + ''.join(gdict[chrom]))
+    sys.stdout.write('>' + chrom + '\n' + ''.join(gdict[chrom]))

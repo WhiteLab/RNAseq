a6663af207fa659e88e6ca08c7016295573c551d
migbro <miguel.a.brown@gmail.com>
2016-05-09 10:59:34 -0500

    Added quick helper script to make an index ids in gtf file

---
+++ utility/gtf2index.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+gtf = sys.argv[1]
+an_type = sys.argv[2]
+
+fh = open(gtf, 'r')
+print 'ID\tName\tType'
+
+idf = an_type + '_id'
+namef = an_type + '_name'
+typef = an_type + '_type'
+(id_name, name, type_name) = ('', '', '')
+for line in fh:
+    if line[0] != '#':
+        info = line.rstrip('\n').split('\t')
+        if info[2] == an_type:
+            id_name = re.search(idf + ' "(\S+)"', info[8])
+            name = re.search(namef + ' "(\S+)"', info[8])
+            type = re.search(typef + ' "(\S+)"', info[8])
+            print '\t'.join(id_name.group(1), name.group(1), type.group(1))
+fh.close()
--- utility/quick_post.py
+++ utility/quick_post.py
@@ -7,7 +7,7 @@ import sys
 # curl -X PUT -H 'Content-Type: application:json' https://128.135.219.167:6984/igsb_qc/24ec4b43cfe304ff4709e76f74004da8?rev=1-48f57c4e6aaba987d0786e6750c19d7c -d @ALIGN_TEST/2014-2241/QC/2014-2241_141126_SN1070_0304_BHB5BPADXX_2.qc_stats.json -k -u "mbrown:Jah6Eeji"
 # ALIGN_TEST/2014-2240/QC/2014-2240_141126_SN1070_0304_BHB5BPADXX_2.qc_stats.json{"ok":true,"id":"24ec4b43cfe304ff4709e76f7400074d","rev":"1-7dd84750ce781a128cfdb7e14ae4d773"}
 curl = 'curl -X PUT -H "Content-Type: application:json" https://128.135.219.167:6984/igsb_qc/'
-login = ' -k -u "mbrown:Jah6Eeji"'
+# TODO replace login string using config file, not hardcoded!!!!
 fh = open(sys.argv[1], 'r')
 setproxy = 'export http_proxy=http://cloud-proxy:3128; export https_proxy=http://cloud-proxy:3128; export no_proxy="rados-bionimbus-pdc.opensciencedatacloud.org";'
 for line in fh:

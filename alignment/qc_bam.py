ad4daeb3cd9ed62f91b09449f64925e05af2a7f0
migbro <miguel.a.brown@gmail.com>
2016-07-09 17:43:51 -0500

    Troubleshooting star wrapper.  Corrected variable in qc_bam

--- alignment/qc_bam.py
+++ alignment/qc_bam.py
@@ -29,8 +29,8 @@ def qc_bam(sample, config_file, ref_mnt):
     refFlat = ref_mnt + '/' + refFlat
     intervals = ref_mnt + '/' + intervals
     picard_cmd = java + ' -Xmx' + ram + 'g ' + picard + ' CollectRnaSeqMetrics REF_FLAT=' + refFlat + ' STRAND=' \
-                 + st_dict[strand] + ' CHART=' + sample + '.pos_v_cov.pdf I=' + sample + '.Aligned.out.bam O=' + sample\
-                 + '.picard_RNAseq_qc.txt RIBOSOMAL_INTERVALS=' + intervals
+                 + st_dict[strand] + ' CHART=' + sample + '.pos_v_cov.pdf I=' + sample \
+                 + '.Aligned.sortedByCoord.out.bam O=' + sample + '.picard_RNAseq_qc.txt RIBOSOMAL_INTERVALS=' + intervals
     # job_list.append(picard_cmd)
     log(loc, date_time() + picard_cmd + '\n')
     subprocess.call(picard, shell=True)
--- alignment/star.py
+++ alignment/star.py
@@ -10,12 +10,13 @@ from log import log
 def star(STAR, genome, end1, end2, sample, log_dir, th, sf):
     loc = log_dir + sample + "star.log"
     meta = sample.split('_')
-    RGRP = "@RG\tID:" + sample + "\tLB:" + meta[0] + "\tPU:" + meta[4] + "\tSM:" + meta[0] + "\tPL:illumina"
+    RGRP = "ID:" + sample + "\tLB:" + meta[0] + "\tPU:" + meta[4] + "\tSM:" + meta[0] + "\tPL:illumina"
     star_cmd = STAR + " --runMode alignReads --twopassMode Basic --outFileNamePrefix " + sample + " --runThreadN " \
                + th + " --genomeDir " + genome + " --readFilesIn " + end1 + " " + end2 + " --readFilesCommand zcat \
                --quantMode TranscriptomeSAM GeneCounts --outSAMtype BAM SortedByCoordinate --outFilterType BySJout \
                --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 8 \
-               --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --outSAMattrRGline " + RGRP
+               --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --outSAMattrRGline \"" + RGRP \
+               + "\""
     if sf == 'N':
         # add XS tag is input is not stranded
         star_cmd += ' --outSAMattributes NH HI AS nM XS'

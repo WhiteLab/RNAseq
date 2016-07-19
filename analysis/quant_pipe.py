52e12311602fe4e90a29dd7adc791a246cf5b3c4
migbro <miguel.a.brown@gmail.com>
2016-07-19 13:32:57 -0500

    Added annotation root in quant pipe

--- analysis/quant_pipe.py
+++ analysis/quant_pipe.py
@@ -2,6 +2,7 @@
 import sys
 import os
 sys.path.append('/home/ubuntu/TOOLS/Scripts/alignment')
+sys.path.append('/home/ubuntu/TOOLS/Scripts/annotation')
 sys.path.append('/home/ubuntu/TOOLS/Scripts/utility')
 from date_time import date_time
 from log import log

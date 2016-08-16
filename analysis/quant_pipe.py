ddaae29c4b7e9405230709080035ecf0833f48e6
migbro <miguel.a.brown@gmail.com>
2016-08-15 22:23:08 -0500

    Corrected upload command for bams and reports

--- analysis/quant_pipe.py
+++ analysis/quant_pipe.py
@@ -35,12 +35,16 @@ def upload_special(bnid, cont, obj):
     bam = bnid + '.merged.transcriptome.bam'
 
     ONE_GB = 1073741824
-    up_bam = src_cmd + ' swift upload -S ' + str(ONE_GB) + ' ' + cont + 'BAMS/' + bam + ' --object-name ' + obj + '/' \
+    up_bam = src_cmd + ' swift upload -S ' + str(ONE_GB) + ' ' + cont + ' BAMS/' + bam + ' --object-name ' + obj + '/' \
              + bnid + '/' + bam
-    subprocess.call(up_bam, shell=True)
-    up_reports = src_cmd + ' swift upload ' + cont + 'REPORTS/  --object-name ' + obj + '/' + bnid + '/REPORTS/'
-    subprocess.call(up_reports, shell=True)
+    check = subprocess.call(up_bam, shell=True)
+    if check != 0:
+        sys.stderr.write('Could not upload bam file. Command given: ' + up_bam + '\n')
 
+    up_reports = src_cmd + ' swift upload ' + cont + ' REPORTS/  --object-name ' + obj + '/' + bnid + '/REPORTS/'
+    check = subprocess.call(up_reports, shell=True)
+    if check != 0:
+        sys.stderr.write('Could not upload report file. Command given: ' + up_reports + '\n')
 
 def quant_pipe(lane, config_file, ref_mnt):
     src_cmd = '. ~/.novarc;'

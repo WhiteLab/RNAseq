344cda3cebc8ab8aa5ac65cdbc0d8707c1ce07cf
migbro <miguel.a.brown@gmail.com>
2016-11-15 16:07:02 -0600

    Fixed bug in outputting unique impacts

--- annotation/vep_report.py
+++ annotation/vep_report.py
@@ -49,10 +49,9 @@ def output_highest_impact(chrom, pos, ref, alt, pos_cov, ann_list, loc_dict, out
                     top_gene = gene
                     f = 1
                     outstring += cur_var
-                    out.write(outstring)
                 if f == 1 and gene != top_gene and impact != 'MODIFIER':
                     outstring += cur_var
-                    out.write(outstring)
+    out.write(outstring)
 
 
 def gen_report(vcf, sample):

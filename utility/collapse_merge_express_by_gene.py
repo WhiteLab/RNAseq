4c8403a814d16b6fed28bfe32e0949bb5957aec9
migbro <miguel.a.brown@gmail.com>
2017-01-13 11:23:04 -0600

    Added ability to limit ro RNA bio type

--- utility/collapse_merge_express_by_gene.py
+++ utility/collapse_merge_express_by_gene.py
@@ -2,10 +2,11 @@
 """
 Combines express output tables, collapsing on gene symbol by effective count
 
-Usage: collapse_merge_express_by_gene.py <ct_list>
+Usage: collapse_merge_express_by_gene.py <ct_list> <type_list>
 
 Arguments:
   <ct_list> list of express count files
+  <type_list> list of transcript types to accept. Type None not to use one
 
 Options:
   -h
@@ -21,6 +22,15 @@ s_list = []
 g_dict = {}
 g_list = []
 tbl_dict = {}
+ty_flag = 'N'
+ty_dict = {}
+
+if args['<type_list>'] != 'None':
+    ty_flag = 'Y'
+    for line in open(args['<type_list>']):
+        line = line.rstrip('\n')
+        ty_dict[line] = 1
+
 for report in open(args['<ct_list>']):
     report = report.rstrip('\n')
     fn = os.path.basename(report)
@@ -31,13 +41,14 @@ for report in open(args['<ct_list>']):
     for line in fh:
         info = line.rstrip('\n').split('\t')
         gene = re.sub('-\d+$', '', info[0])
-        if gene not in g_dict:
-            g_list.append(gene)
-            g_dict[gene] = 1
-            tbl_dict[gene] = {}
-        if parts[0] not in tbl_dict[gene]:
-            tbl_dict[gene][parts[0]] = 0.0
-        tbl_dict[gene][parts[0]] += float(parts[9])
+        if ty_flag == 'N' or info[1] in ty_dict:
+            if gene not in g_dict:
+                g_list.append(gene)
+                g_dict[gene] = 1
+                tbl_dict[gene] = {}
+            if parts[0] not in tbl_dict[gene]:
+                tbl_dict[gene][parts[0]] = 0.0
+            tbl_dict[gene][parts[0]] += float(info[9])
     fh.close()
 
 print 'Sample/Gene\t' + '\t'.join(s_list)

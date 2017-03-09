3daa81abbf52aff7373a94d20b2b67ab392ecde8
migbro <miguel.a.brown@gmail.com>
2017-03-09 16:57:15 -0600

    Created a temp pipe to repeat qc_bam and parse qc to update qc with new rRNA reference

--- temp_pipelines/qc_bam_pipe.py
+++ temp_pipelines/qc_bam_pipe.py
@@ -4,31 +4,33 @@ sys.path.append('/home/ubuntu/TOOLS/Scripts/')
 import json
 from utility.job_manager import job_manager
 
+
 def parse_config(config_file):
     config_data = json.loads(open(config_file, 'r').read())
     return config_data['refs']['cont'], config_data['refs']['obj']
 
 
-def qc_bam_pipe(sample, config_file, ref_mnt):
+def qc_bam_pipe(sample_list, config_file, ref_mnt):
     (cont, obj) = parse_config(config_file)
     job_list = []
     log_dir = 'LOGS/'
     src_cmd = '. ~/.novarc;'
-    for samp in open(sample):
-        samp = samp.rstrip('\n')
-        parts = samp.split('_')
-        bam = samp + '.Aligned.sortedByCoord.out.bam'
-        dl_list = (log_dir + samp + '.cutadapt.log', log_dir + samp + '.Log.final.out', 'QC/' + samp
-                   + '_subset.insert_metrics.hist', 'QC/' + samp + '_1_sequence_fastqc/fastqc_data.txt', 'BAMS/'
+    for sample in open(sample_list):
+        sample = sample.rstrip('\n')
+        parts = sample.split('_')
+        bam = sample + '.Aligned.sortedByCoord.out.bam'
+        dl_list = (log_dir + sample + '.cutadapt.log', log_dir + sample + '.Log.final.out', 'QC/' + sample
+                   + '_subset.insert_metrics.hist', 'QC/' + sample + '_1_sequence_fastqc/fastqc_data.txt', 'BAMS/'
                    + bam)
         dl_cmd = ''
         prefix = obj + '/' + parts[0] + '/'
         for fn in dl_list:
             dl_cmd += src_cmd + 'swift download ' + cont + ' ' + prefix + fn + ';'
         mv_cmd = 'mv ' + prefix + dl_list[2] + ' .;mv ' + prefix + dl_list[4] + ' .;'
-        qc_cmd = '~/TOOLS/Scripts/alignment/qc_bam.py -sa ' + samp + ' -j ' + config_file + ' -m ' + ref_mnt + ';'
+        qc_cmd = '~/TOOLS/Scripts/alignment/qc_bam.py -sa ' + sample + ' -j ' + config_file + ' -m ' + ref_mnt + ';'
         rm_cmd = 'rm ' + bam + ';'
-        full_cmd = dl_cmd + mv_cmd + qc_cmd + rm_cmd
+        parse_qc_cmd = '~/TOOLS/Scripts/alignment/qc_bam.py -j ' + config_file + ' -sa ' + sample + ';'
+        full_cmd = dl_cmd + mv_cmd + qc_cmd + rm_cmd + parse_qc_cmd
         job_list.append(full_cmd)
     job_manager(job_list, 4)
 

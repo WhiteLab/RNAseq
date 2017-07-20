913aee4b7109fadf557bab504a38380495c41dd8
migbro <miguel.a.brown@gmail.com>
2017-07-20 17:58:28 -0500

    Added to collapse script ability to pick a different column to collapse on

--- utility/collapse_merge_express_by_gene.py
+++ utility/collapse_merge_express_by_gene.py
@@ -2,11 +2,12 @@
 """
 Combines express output tables, collapsing on gene symbol by effective count
 
-Usage: collapse_merge_express_by_gene.py <ct_list> <type_list>
+Usage: collapse_merge_express_by_gene.py <ct_list> <type_list> <field>
 
 Arguments:
   <ct_list> list of express count files
   <type_list> list of transcript types to accept. Type None not to use one
+  <field> name of field to collapse on or default for est_counts
 
 Options:
   -h
@@ -38,6 +39,18 @@ for report in open(args['<ct_list>']):
     s_list.append(parts[0])
     fh = open(report)
     head = next(fh)
+    head_vals = head.rstrip('\n').split('\t')
+    f = 9
+    if args['<field>'] != 'default':
+        flag = 0
+        for i in xrange(len(head_vals)):
+            if head_vals[i] == args['<field>']:
+                f = i
+                flag = 1
+                break
+        if flag == 0:
+            sys.stderr.write('Field name ' + args['<field>'] + ' not found.  Using default!\n')
+
     for line in fh:
         info = line.rstrip('\n').split()
         gene = re.sub('-\d+$', '', info[0])
@@ -48,7 +61,7 @@ for report in open(args['<ct_list>']):
                 tbl_dict[gene] = {}
             if parts[0] not in tbl_dict[gene]:
                 tbl_dict[gene][parts[0]] = 0.0
-            tbl_dict[gene][parts[0]] += float(info[9])
+            tbl_dict[gene][parts[0]] += float(info[f])
     fh.close()
 
 print 'Sample/Gene\t' + '\t'.join(s_list)

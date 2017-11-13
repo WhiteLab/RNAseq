8d50c5bb98ba3ff2255d775f37926d8eb3c4b793
migbro <miguel.a.brown@gmail.com>
2017-11-13 15:26:16 -0600

    Created a downsample script for use on the cluster.  Needs wrapper script

---
+++ temp_pipelines/downsample_bams.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('/cephfs/users/mbrown/RNAseq')
+from utility.date_time import date_time
+import subprocess
+
+
+def downsample_bams(samtools, bam, frac, out_dir, th):
+    out_root = bam.replace('.bam','')
+    cmd = samtools + ' view --threads ' + th + ' -b ' + bam + ' -s ' + frac + ' > ' + out_dir + '/' + out_root \
+                                          + '_subsample_ ' + frac + '.bam'
+    sys.stderr.write(date_time() + 'Downsampling ' + bam + '\n' + cmd + '\n')
+    subprocess.call(cmd, shell=True)
+    sys.stderr.write(date_time() + 'process complete!\n')
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Downsample bams for RNAseq anaylsis')
+    parser.add_argument('-s', '--samtools', action='store', dest='samtools', help='samtools location')
+    parser.add_argument('-b', '--bam_file', action='store', dest='bam', help='bam file name')
+    parser.add_argument('-f', '--fraction', action='store', dest='frac',
+                        help='Relative frequency of total reads to output')
+    parser.add_argument('-o', '--output_dir', action='store', dest='out_dir',
+                        help='Location to output resulting bam')
+    parser.add_argument('-t', '--threads', action='store', dest='threads',
+                        help='Num threads to use')
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    inputs = parser.parse_args()
+    (samtools, bam, frac, out_dir, th) = (inputs.samtools, inputs.bam, inputs.frac, inputs.out_dir, inputs.threads)
+    downsample_bams(samtools, bam, frac, out_dir, th)
